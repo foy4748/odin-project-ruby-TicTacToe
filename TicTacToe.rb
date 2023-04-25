a4ca77361ccc463548dfa8f13d9b381977f56482
foy4748 <faisaljfcl@gmail.com>
2023-04-25 15:35:36 +0600

    Putting marks on selected numbered space

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -10,6 +10,34 @@ class TicTacToe
     puts "\nPlayer 1: X || Player 2: O \n"
   end
 
+  # Putting Marks
+  def put_mark()
+    print_board()
+
+    idx = gets().chomp
+    index = idx.to_i - 1
+
+    # Putting marks 
+    # based on switch state
+    if switch
+      if !board[index]
+        self.board[index] = player1
+        @switch = false
+      else
+        puts "\n #{idx} is Already occupied \n"
+      end
+    else
+      if !board[index]
+        self.board[index] = player2
+        @switch = true
+      else
+        puts "\n #{idx} is Already occupied \n"
+      end
+    end
+
+    put_mark()
+  end
+
   # Display Methods
   def to_s()
     " Current Game: #{@board}\n Player 1 : #{@player1} \n Player 2 : #{@player2}"
@@ -19,7 +47,7 @@ class TicTacToe
     b = ""
     puts "\n"
     self.board.each_with_index do |item, index|
-      b += "#{item || index + 1}#{(index + 1) % size == 0 ? "\n----------\n" : " | "}"
+      b += "#{item || index + 1}#{(index + 1) % 3 == 0 ? "\n----------\n" : " | "}"
     end
 
     puts b
@@ -29,3 +57,4 @@ end
 
 game = TicTacToe.new
 puts game
+game.put_mark
