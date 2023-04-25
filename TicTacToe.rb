1f87128902b3694412b8b7b7093a8381727733a7
foy4748 <faisaljfcl@gmail.com>
2023-04-25 16:35:15 +0600

    Added Winning Logic - Column Scan
    Scan through the column at the marked space.
    I call the scanning space "tray"
    
    Checks whether all the marks at the column
    are same as the mark of the current player.
    Returns Boolean
    
    Added Winning Logic - Diagonal Scan
    Scan through the diagonal from
    Left  -> Right
    and
    Right -> Left
    
    Checks whether all the marks at the both diagonal
    are same as the mark of the current player.
    Returns Boolean
    
    Now changed the dimension from hardcoded 3x3 to
    size x size. The default is 3x3
    
    Added a Helper Method - all_same?
    
    Checks whether all the elements of "tray"
    are 0 or not. Returns boolean
    
    Checks if all the elements of a

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -1,14 +1,74 @@
 class TicTacToe
-  attr_reader :board, :player1, :player2, :switch
+  attr_reader :board, :player1, :player2, :switch, :size
 
-  def initialize()
+  def initialize(s=3)
     @board = Array.new(9,nil)
     @player1 = "X"
     @player2 = "O"
     @switch = true
+    @size = s
 
     puts "\nPlayer 1: X || Player 2: O \n"
     puts "\nNow its #{switch ? player1 : player2}\'s turn"
+  end
+
+  # Helper methods
+
+  ## Checking whether all 
+  ## of the element in the 
+  ## tray is X or O
+  def all_same?(tray)
+    tray.all?{|elm| elm == 0}
+  end
+
+  # Winning Logics
+
+  ## Column Scan
+  def column_scan?(idx)
+    tray = []
+    idx = idx.to_i
+    initial = board[idx - 1]
+
+    for i in 1..(size-1)
+      if idx + i*size > size*size
+        nxt = idx + i*size - size*size
+      else
+        nxt = idx + i*size
+      end
+      c = initial <=> (board[nxt-1] || "A")
+      tray.push(c)
+    end
+    all_same?(tray)
+  end
+
+  ## Diagonal Scan
+  def diagonal_scan?()
+    result = []
+    # From Left -> Right
+    start = 1
+    initial = board[start - 1]
+    tray1 = []
+
+    for i in 1..(size - 1)
+      nxt = start + i*(size + 1)
+      c = initial <=> (board[nxt - 1] || "A")
+      tray1.push(c)
+    end
+    result.push(all_same?(tray1))
+
+    # From Right -> Left
+    start = size
+    initial = board[start - 1]
+    tray2 = []
+
+    for i in 1..(size - 1)
+      nxt = start + i*(size - 1)
+      c = initial <=> (board[nxt - 1] || "A")
+      tray2.push(c)
+    end
+    result.push(all_same?(tray2))
+
+    result.any?{|elm| elm == true}
   end
 
   # Putting Marks
@@ -36,6 +96,11 @@ class TicTacToe
       end
     end
 
+    # Scanning through 
+    # board for Winning combo
+    p column_scan?(idx)
+    p diagonal_scan?()
+
     puts "\nNow its #{switch ? player1 : player2}\'s turn"
     put_mark()
   end
@@ -49,7 +114,7 @@ class TicTacToe
     b = ""
     puts "\n"
     self.board.each_with_index do |item, index|
-      b += "#{item || index + 1}#{(index + 1) % 3 == 0 ? "\n----------\n" : " | "}"
+      b += "#{item || index + 1}#{(index + 1) % size == 0 ? "\n----------\n" : " | "}"
     end
 
     puts b
